b8c4b03d8558b5f34206cc52e52a5a3950abeda4
katyastan <134015338+katyastan@users.noreply.github.com>
2023-08-02 18:11:54 +0300

    homework19

--- homework19/test/specs/elementPage.test.js
+++ homework19/test/specs/elementPage.test.js
@@ -4,11 +4,11 @@ describe('The Element Object', function() {
   it('Сhecking the title of the element documentation page', async function() {
     await browser.url('https://webdriver.io/');
     await $('//a[contains(@class,"navbar__link") and @href="/docs/api"]').click();
-    await $('//div[@class="theme-doc-markdown markdown"]//h1').waitForDisplayed();
+    await $('//h2[@id="contribute"]/..//h1').waitForDisplayed();
     await $('//a[contains(@class,"menu__link--sublist") and @href="/docs/api/element"]').click();
-    await $('//div[@class="theme-doc-markdown markdown"]//h1').waitForDisplayed();
+    await $('//div[@class="col docItemCol_VOVn"]//h1').waitForDisplayed();
     expect(
-      await $('//div[@class="theme-doc-markdown markdown"]//h1').getText(),
+      await $('//div[@class="col docItemCol_VOVn"]//h1').getText(),
     ).to.equal('The Element Object');
   });
 });
