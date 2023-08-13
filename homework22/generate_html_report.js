e2c913532f8b1c7abfb8c82f1b3790e191528ffd
katyastan <134015338+katyastan@users.noreply.github.com>
2023-08-13 22:04:53 +0300

    report

--- homework22/generate_html_report.js
+++ homework22/generate_html_report.js
@@ -1,6 +1,6 @@
-var reporter = require('cucumber-html-reporter');
+const reporter = require('cucumber-html-reporter');
 
-var options = {
+const options = {
     theme: 'bootstrap',
     jsonDir: 'test_report/',
     output: 'test_report/cucumber_report.html',
